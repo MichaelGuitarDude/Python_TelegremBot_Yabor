be7924e83821cc72b9c841622d5f8ebd401f3dad
MichaelGuitarDude <michaelguitardude@gmail.com>
2023-11-01 22:58:00 +0500

    note_app final version 1.0 with colorama

--- task3_note_app.py
+++ task3_note_app.py
@@ -1,25 +1,57 @@
 import os
+from colorama import init, Fore, Style
+init(autoreset=True)
+
+
 
 def build_note(note_text, note_name):
     with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
         file.write(note_text)
-    print(f"Заметка {note_name} создана.")
+    print(Style.BRIGHT + Fore.GREEN + f"Заметка {note_name} создана.\n")
+
 
 def create_note():
     note_name = input("Введите название заметки: ")
     note_text = input("Введите текст заметки: ")
     build_note(note_text, note_name)
 
-def read_note(note_name):
-    with open(f"{note_name}.txt", "r", encoding="utf-8") as file:
-        data = file.read()
-    print(f'Текст заметки: {data}\n')
+
+def read_note():
+    note_name = input("Введите название заметки: ")
+    if os.path.isfile(f"{note_name}.txt"):
+        with open(f"{note_name}.txt", "r", encoding="utf-8") as file:
+            note_text = file.read()
+        print(Style.BRIGHT + Fore.GREEN + f'Текст заметки: {note_text}\n')
+    else: 
+        print(Fore.RED + 'Заметка не найдена.\n')
+
 
 def edit_note():
-    ...
+    note_name = input("Введите название заметки: ")
+    if os.path.isfile(f"{note_name}.txt"):
+
+        # Читаем файл и выводим на экран
+        with open(f"{note_name}.txt", "r", encoding="utf-8") as file:
+            note_text = file.read()
+        print(Style.BRIGHT + Fore.BLUE + f'Старый текст заметки: {note_text}')
+
+        # Перезаписываем файл и тоже выводим на экран
+        with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+            note_text = input('Введите новый текст: ')
+            file.write(note_text)
+        print(Style.BRIGHT + Fore.GREEN + f'Новый текст заметки: {note_text}\n')
+    else: 
+        print(Fore.RED + 'Заметка не найдена.\n')
+
 
 def delete_note():
-    ...
+    note_name = input("Введите название заметки: ")
+    if os.path.isfile(f"{note_name}.txt"):
+        os.remove(f"{note_name}.txt")
+        print(Style.BRIGHT + Fore.RED + 'Файл удалён!\n')
+    else: 
+        print(Fore.RED + 'Заметка не найдена.\n')
+
 
 def main():
     key = True
@@ -34,14 +66,15 @@ def main():
             print(f'{k} : {menu.get(k)}')
         
         key = input('\nВведите ключ операции, если хотите выйти, введите любые другие символы: ',)
-        if key == ('1' or '2' or '3' or '4'):
-            print(f'Выбрана операция: {menu[key]}\n')
+        if key == '1' or key == '2' or key == '3' or key == '4':
+            print(Style.BRIGHT + Fore.CYAN + f'Выбрана операция: {menu[key]}\n')
+        else:
+            print(Style.BRIGHT + Fore.CYAN + 'Работа программы завершена.\n')
 
         if key == '1':
             create_note()
         elif key == '2':
-            note_name = input("Введите название заметки: ")
-            read_note(note_name)
+            read_note()
         elif key == '3':
             edit_note()
         elif key == '4':
@@ -49,6 +82,5 @@ def main():
         else:
             key = False
 
-    #create_note()
 
 main()
